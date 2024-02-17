d1b612cc3843a9c31fe15f5d0eff51a4484c233d
RENATU BONFIM <renatu_mb@yahoo.com.br>
2024-02-17 02:43:38 -0800

    Adjustments for User - Entity (Omit password ) - Controller ( Swagger) - Service ( Creation of getUserByEmail() method )

--- backend/src/main/java/org/example/recipesbackend/service/UserService.java
+++ backend/src/main/java/org/example/recipesbackend/service/UserService.java
@@ -37,4 +37,8 @@ public class UserService {
     public User getUserById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
     }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException(0L));
+    }
 }
